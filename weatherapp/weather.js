7eb8638fdea719983bb80472318efec0548d832d
Zach Marschik <zmarschik@gmail.com>
2016-10-05 11:15:28 -0500

    added time

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -7,21 +7,27 @@ $.getJSON('http://ipinfo.io', function(data){
   var zipcode = data.postal;
 
   var getWeather = function(){
-	var url = "HTTP://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",{api}";
+	var url = "HTTP://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&APPID=2b16187d82ecabb9d5259af118f91199";
 	$.getJSON(url, function(weather_data){
 	  var tempF = Math.floor((((weather_data.main.temp)*9)/5)-459.67);
 	  var tempC = Math.floor(weather_data.main.temp - 273.15);
-	  var weather = weather_data.weather[0].main;
-	  var description = weather_data.weather[0].description;
-	  var sun = {"sunrise": weather_data.sys.sunrise, "sunset": weather_data.sys.sunset}
 	  var windSpeed = ((weather_data.wind.speed)*2.23694).toFixed(2);
 	  var windAngle = Math.floor(weather_data.wind.deg);
 	  var weatherId = weather_data.weather[0].id;
-	  console.log(tempF);
+
+	  var getTime = function(){
+	  	var date = new Date();
+	    var hours = date.getHours();
+	    var mins = date.getMinutes();
+
+	    return hours+":"+mins;
+	  }
+
+	  
 	  $(".one").append("<div class = 'text-container'><p>"+tempF+"&degF</p></div>");
 	  $(".two").append("<div class = 'text-container'><i class = 'wi wi-owm-"+weatherId+"'></i></div>");
 	  $(".three").append("<div class = 'text-container'><p>"+windSpeed+"mph<i class = 'wi wi-wind towards-"+windAngle+"-deg'></i></p></div>");
-	  //$("#circle-temp").append("<p>"+tempC+"&#8451</p>");
+	  $(".four").append("<div class = 'text-container'><p>"+getTime()+"</p></div>");
 
 	  
 	  /*$("#temp").append("<p>"+tempF+"</p>");
