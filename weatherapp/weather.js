2dc250daf440161d7f13af9b9d9cc8ea7da74e4e
Zach <zmarschik@gmail.com>
2016-10-05 20:59:58 -0500

    adding in google embed map in center circle.

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -5,15 +5,18 @@ $.getJSON('http://ipinfo.io', function(data){
   var region = data.region;
   var country = data.country;
   var zipcode = data.postal;
+  var loc = data.loc;
 
   var getWeather = function(){
 	var url = "HTTP://api.openweathermap.org/data/2.5/weather?zip="+zipcode+",us&APPID=2b16187d82ecabb9d5259af118f91199";
+	var mapUrl = "https://www.google.com/maps/embed/v1/view?key=AIzaSyCrgH1YBfVDLiYmIMPhaD34YtMqOTU9xcg&center="+loc+"&zoom=14"
 	$.getJSON(url, function(weather_data){
 	  var tempF = Math.floor((((weather_data.main.temp)*9)/5)-459.67);
 	  var tempC = Math.floor(weather_data.main.temp - 273.15);
 	  var windSpeed = ((weather_data.wind.speed)*2.23694).toFixed(2);
 	  var windAngle = Math.floor(weather_data.wind.deg);
 	  var weatherId = weather_data.weather[0].id;
+
 
 	  var getTime = function(){
 	  	var date = new Date();
@@ -28,6 +31,7 @@ $.getJSON('http://ipinfo.io', function(data){
 	  $(".two").append("<div class = 'text-container'><i class = 'wi wi-owm-"+weatherId+"'></i></div>");
 	  $(".three").append("<div class = 'text-container'><p>"+windSpeed+"mph<i class = 'wi wi-wind towards-"+windAngle+"-deg'></i></p></div>");
 	  $(".four").append("<div class = 'text-container'><p>"+getTime()+"</p></div>");
+	  $("#circle-big").append("<iframe src="+mapUrl+"></iframe>");
 
 	  
 	  /*$("#temp").append("<p>"+tempF+"</p>");
